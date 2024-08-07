e4db3deef018135c8bf591baaac3d5e27944eb71
unknown <you@example.com>
2024-08-07 15:12:51 +0800

    update

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,9 +4,10 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-	<script src="http://18.163.86.151/assets/layer/ai_service_diy_3.js" charset="UTF-8"></script>
+	<script src="http://18.163.86.151/assets/layer/ai_service_diy_3.js?v=1723014071" charset="UTF-8"> </script>
 </head>
 <body>
+	<div><a href="http://18.163.86.151/index/index/home?visiter_id=&visiter_name=&avatar=&business_id=3&groupid=2&special=12">客服測試</a></div>
 
 </body>
 </html>
