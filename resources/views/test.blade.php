b57879ef3b7a8b8027d62f03be1487f3bb401c96
unknown <you@example.com>
2024-08-07 14:40:02 +0800

    update test html

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,28 +1,12 @@
-server {
-    listen 80;
-    listen [::]:80;
-    server_name waitplusegg.com www.waitplusegg.com;
-    return 301 https://$server_name$request_uri;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<script src="http://18.163.86.151/assets/layer/ai_service_diy_3.js" charset="UTF-8"></script>
+</head>
+<body>
 
-server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-    server_name waitplusegg.com www.waitplusegg.com;
-
-    ssl_certificate /etc/letsencrypt/live/waitplusegg.com/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/waitplusegg.com/privkey.pem;
-
-    root /bitnami/wordpress; 
-    index index.php index.html index.htm;
-
-    location / {
-        try_files $uri $uri/ /index.php?$args;
-    }
-
-    location ~ \.php$ {
-        fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;  # 根據您的PHP版本調整
-        fastcgi_index index.php;
-        include fastcgi_params;
-    }
-}
+</body>
+</html>
