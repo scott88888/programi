6754cae456e7147c787d35b4eda8c7dce67e825f
unknown <you@example.com>
2024-08-09 14:59:14 +0800

    update ass

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,11 +16,11 @@
     <link rel="apple-touch-icon" sizes="114x114" href="images/programilogo114114.png"> 
     <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-    <link rel="stylesheet" href="asset('css/vendors.min.css')" />
-    <link rel="stylesheet" href="asset('css/icon.min.css')" />
-    <link rel="stylesheet" href="asset('css/style.css')" />
-    <link rel="stylesheet" href="asset('css/responsive.css')" />
-    <link rel="stylesheet" href="asset('demos/seo-agency/seo-agency.css')" />
+    <link rel="stylesheet" href="{{asset('/css/vendors.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('/css/icon.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}" />
+    <link rel="stylesheet" href="{{asset('/css/responsive.css')}}" />
+    <link rel="stylesheet" href="{{asset('/demos/seo-agency/seo-agency.css')}}" />
 
 
 
@@ -100,9 +100,9 @@
 </div>
 <!-- end scroll progress -->
 <!-- javascript libraries -->
-<script type="text/javascript" src="asset('js/jquery.js')";></script>
-<script type="text/javascript" src="asset('js/vendors.min.js')"></script>
-<script type="text/javascript" src="asset('js/main.js')"></script>
+<script type="text/javascript" src="{{asset('js/jquery.js')}}";></script>
+<script type="text/javascript" src="{{asset('js/vendors.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/main.js')}}"></script>
 
 </body>
 </html>
